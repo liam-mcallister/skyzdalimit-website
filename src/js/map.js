51e7c6dccdf096b5beb8633199af448aab185f2b
Liam McAllister <liammca1987@gmail.com>
2021-06-17 17:02:29 +0100

    created production page / added leaflet map

---
+++ src/js/map.js
@@ -0,0 +1,26 @@
+const myMap = L.map("mapid").setView([54.59808, -7.28408], 16);
+
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution: "Map data &copy; OpenStreetMap, Imagery &copy; Mapbox",
+    maxZoom: 18,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: "",
+  }
+).addTo(myMap);
+
+const blueIcon = new L.Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+L.marker([54.59808, -7.28408], { icon: blueIcon }).addTo(myMap);
