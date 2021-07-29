c04acf87fee4032f1aec26ff5e21ffbda25cd7d3
Liam McAllister <liammca1987@gmail.com>
2021-07-29 17:33:48 +0100

    Changed cast section to a carousel using glider.js

--- src/js/app.js
+++ src/js/app.js
@@ -9,3 +9,49 @@ burger.addEventListener("click", function () {
     body.classList.remove("no-scroll");
   }
 });
+
+new Glider(document.querySelector(".glider"), {
+  // Mobile-first defaults
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  draggable: true,
+  rewind: true,
+  arrows: {
+    prev: ".glider-prev",
+    next: ".glider-next",
+  },
+  responsive: [
+    {
+      // screens greater than >= 500px
+      breakpoint: 500,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        itemWidth: 150,
+        duration: 0.25,
+      },
+    },
+    {
+      // screens greater than >= 700px
+      breakpoint: 700,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        itemWidth: 150,
+        duration: 0.25,
+      },
+    },
+    {
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        itemWidth: 150,
+        duration: 0.25,
+      },
+    },
+  ],
+});
