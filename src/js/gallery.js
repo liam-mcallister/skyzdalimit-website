97b77a983ef4c6cde0fcc1345e20e283199477bc
liam-mcallister <liammca1987@gmail.com>
2021-11-08 16:31:39 +0000

    Disabled scrolling when gallery is open

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -2,6 +2,7 @@ const baguetteBox = require("baguettebox.js");
 
 window.addEventListener("load", function () {
     baguetteBox.run(".gallery", {
-        animation: 'fadeIn'
+        animation: 'fadeIn',
+        bodyClass: 'no-scroll'
     });
 });
